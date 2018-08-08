b50e27277a56899d994e2852e54776958a887ab9
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-08-07 18:51:47 -0700

    Replace all dreamlab urls to be relative links

--- websites/dreamlab/ajax/apps/formSubmitAjax.php
+++ websites/dreamlab/ajax/apps/formSubmitAjax.php
@@ -1,3 +1,3 @@
 <!DOCTYPE html>
 <html>
-<head><script type="text/javascript">function ret(){parent.postMessage && parent.postMessage(document.body.firstChild.nodeValue,'http://www.dreamlab.com.au');}</script></head><body onload='ret()'>{"success":true,"action":"finished","data":{"message":null,"ucfid":0}}</body></html>
+<head><script type="text/javascript">function ret(){parent.postMessage && parent.postMessage(document.body.firstChild.nodeValue,'.');}</script></head><body onload='ret()'>{"success":true,"action":"finished","data":{"message":null,"ucfid":0}}</body></html>
