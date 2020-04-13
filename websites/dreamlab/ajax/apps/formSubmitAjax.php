7452a62767e02236f6273580f265fce7426e391d
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-04-13 01:02:41 -0700

    Copy over new website built with Portfolio Generator

--- websites/dreamlab/ajax/apps/formSubmitAjax.php
+++
@@ -1,3 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head><script type="text/javascript">function ret(){parent.postMessage && parent.postMessage(document.body.firstChild.nodeValue,'.');}</script></head><body onload='ret()'>{"success":true,"action":"finished","data":{"message":null,"ucfid":0}}</body></html>
